820439471a6300ca97a3ab957b89a7bf4721bf4f
santhoshkumar A <santhoshkumara1204@gmail.com>
2021-05-02 22:31:28 +0530

    Basic calculator project using java

---
+++ main_calculator.java
@@ -0,0 +1,10 @@
+package calculatorr;
+
+public class main_calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculator cals=new calculator();
+	}
+
+}
